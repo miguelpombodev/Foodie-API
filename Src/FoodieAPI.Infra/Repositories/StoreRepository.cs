82ac91cdeb803ee17af8d4c4ee149e6e8bb8ab7c
Miguel Pombo <miguel.pomboshape@modec.com>
2024-08-03 17:01:31 -0300

    fix: changing get stores list endpoint response to only what the app needs

--- Src/FoodieAPI.Infra/Repositories/StoreRepository.cs
+++ Src/FoodieAPI.Infra/Repositories/StoreRepository.cs
@@ -1,5 +1,6 @@
 using FoodieAPI.Domain.DTO.Responses;
 using FoodieAPI.Domain.Entities;
+using FoodieAPI.Domain.DTO.Responses;
 using FoodieAPI.Domain.Interfaces.Repositories;
 using FoodieAPI.Infra.Context;
 using Microsoft.EntityFrameworkCore;
@@ -8,69 +9,69 @@ namespace FoodieAPI.Infra.Repositories;
 
 public class StoreRepository : IStoreRepository
 {
-    private readonly DataContext _dataContext;
+  private readonly DataContext _dataContext;
 
-    public StoreRepository(DataContext dataContext)
-    {
-        _dataContext = dataContext;
-    }
+  public StoreRepository(DataContext dataContext)
+  {
+    _dataContext = dataContext;
+  }
 
-    public async Task<List<StoreCategory>> GetStoreCategoriesListAsync()
-    {
-        var storeCategoriesList = await _dataContext.Set<StoreCategory>().ToListAsync();
+  public async Task<List<StoreCategory>> GetStoreCategoriesListAsync()
+  {
+    var storeCategoriesList = await _dataContext.Set<StoreCategory>().ToListAsync();
 
-        return storeCategoriesList;
-    }
+    return storeCategoriesList;
+  }
 
-    public async Task<List<StoreType>> GetStoreTypesListAsync()
-    {
-        var storeTypesList = await _dataContext.Set<StoreType>().Where(st => st.Avatar != null).ToListAsync();
+  public async Task<List<StoreType>> GetStoreTypesListAsync()
+  {
+    var storeTypesList = await _dataContext.Set<StoreType>().Where(st => st.Avatar != null).ToListAsync();
 
-        return storeTypesList;
-    }
+    return storeTypesList;
+  }
 
-    public async Task<List<ListStoreResponseDTO>> GetStoreListAsync(
-        string? sortByOptionName,
-        decimal? sortByDeliveryFee
-    )
-    {
-        var joinStoresList = _dataContext.Set<Store>().Join(
-            _dataContext.Set<StoreType>(),
-            store => store.StoreTypeId,
-            storeType => storeType.Id,
-            (store, storeType) => new
-            {
-                storeAvatar = store.Avatar,
-                storeName = store.Name,
-                storeTypeName = storeType.Name,
-                storeRate = store.StoreRate,
-                storeMinDeliveryTime = store.StoreMinDeliveryTime,
-                storeMaxDeliveryTime = store.StoreMaxDeliveryTime,
-                storeDeliveryFee = store.StoreDeliveryFee
-            }
-        );
-
-        if (sortByOptionName != null)
+  public async Task<List<ListStoreResponseDTO>> GetStoreListAsync(
+      string? sortByOptionName,
+      decimal? sortByDeliveryFee
+  )
+  {
+    var joinStoresList = _dataContext.Set<Store>().Join(
+        _dataContext.Set<StoreType>(),
+        store => store.StoreTypeId,
+        storeType => storeType.Id,
+        (store, storeType) => new
         {
-            joinStoresList = sortByOptionName switch
-            {
-                "sortByRating" => joinStoresList.OrderByDescending(x => x.storeRate),
-                "sortByDeliveryFee" => joinStoresList.OrderBy(x => x.storeDeliveryFee),
-                "sortByDeliverTime" => joinStoresList.OrderBy(x => x.storeMinDeliveryTime),
-                _ => throw new ArgumentOutOfRangeException(nameof(sortByOptionName), sortByOptionName, null)
-            };
+          storeAvatar = store.Avatar,
+          storeName = store.Name,
+          storeTypeName = storeType.Name,
+          storeRate = store.StoreRate,
+          storeMinDeliveryTime = store.StoreMinDeliveryTime,
+          storeMaxDeliveryTime = store.StoreMaxDeliveryTime,
+          storeDeliveryFee = store.StoreDeliveryFee
         }
+    );
 
-        if (sortByDeliveryFee != null)
-        {
-            joinStoresList = joinStoresList.Where(query => query.storeDeliveryFee == sortByDeliveryFee);
-        }
-
-
-        var storesList = await joinStoresList.Select(query =>
-                new ListStoreResponseDTO(query.storeAvatar, query.storeName, query.storeTypeName, query.storeRate, query.storeMinDeliveryTime, query.storeMaxDeliveryTime, query.storeDeliveryFee))
-            .ToListAsync();
-
-        return storesList;
+    if (sortByOptionName != null)
+    {
+      joinStoresList = sortByOptionName switch
+      {
+        "sortByRating" => joinStoresList.OrderByDescending(x => x.storeRate),
+        "sortByDeliveryFee" => joinStoresList.OrderBy(x => x.storeDeliveryFee),
+        "sortByDeliverTime" => joinStoresList.OrderBy(x => x.storeMinDeliveryTime),
+        _ => throw new ArgumentOutOfRangeException(nameof(sortByOptionName), sortByOptionName, null)
+      };
     }
+
+    if (sortByDeliveryFee != null)
+    {
+      joinStoresList = joinStoresList.Where(query => query.storeDeliveryFee == sortByDeliveryFee);
+    }
+
+
+    var storesList = await joinStoresList.Select(query =>
+            new ListStoreResponseDTO(query.storeAvatar, query.storeName, query.storeTypeName, query.storeRate, query.storeMinDeliveryTime, query.storeMaxDeliveryTime, query.storeDeliveryFee))
+        .ToListAsync();
+
+    return storesList;
+  }
 }
