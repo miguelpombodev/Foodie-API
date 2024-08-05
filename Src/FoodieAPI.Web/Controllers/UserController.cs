cc2bf5a44a9b1fa12acfa010d29caf4e1717ee3b
Miguel Pombo <miguel.pomboshape@modec.com>
2024-08-05 19:04:17 -0300

    feat: creating data encrypting service and implementing for users sensible infos

--- Src/FoodieAPI.Web/Controllers/UserController.cs
+++ Src/FoodieAPI.Web/Controllers/UserController.cs
@@ -1,30 +1,32 @@
+using FoodieAPI.Domain;
 using FoodieAPI.Domain.DTO.Requests;
 using FoodieAPI.Domain.Interfaces.Services;
+using FoodieAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodieAPI.Web.Controllers
 {
   [Route("account")]
   [ApiController]
-  public class UserController : ControllerBase
+  public class UserController(IUserService userService, IDataEncryptionService encryptionService) : ControllerBase
   {
-    private readonly IUserService _service;
-    public UserController(IUserService userService)
-    {
-      _service = userService;
-    }
+    private readonly IDataEncryptionService _encryptionService = encryptionService;
+    private readonly IUserService _service = userService;
 
     [HttpPost("v1/create")]
     public async Task<IActionResult> CreateOneUserAsync(
       [FromBody] CreateUserDTO body
     )
     {
-      var usersList = await _service.CreateOneUserAsync(
+      body.CPF = _encryptionService.Hash(body.CPF);
+      body.Phone = _encryptionService.Hash(body.Phone);
+
+      var result = await _service.CreateOneUserAsync(
         body
       );
 
       return StatusCode(
-        StatusCodes.Status200OK, new { usersList }
+        StatusCodes.Status200OK, new { result }
       );
     }
 
--- Src/FoodieAPI.Web/Startup.cs
+++ Src/FoodieAPI.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace FoodieAPI.Web
       services.AddScoped<IUserService, UserService>();
       services.AddScoped<IStoreService, StoreService>();
       services.AddScoped<IProductsService, ProductService>();
+      services.AddSingleton<IDataEncryptionService, DataEncryptionService>();
 
       services.AddCors(options =>
           {
