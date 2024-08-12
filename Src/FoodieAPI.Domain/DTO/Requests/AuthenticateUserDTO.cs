6c72b4a29ebe975b76aae8b1ba78535da324433f
Miguel Pombo <miguel.pomboshape@modec.com>
2024-08-12 13:17:33 -0300

    fix: returning publich folder to out

--- Src/FoodieAPI.Domain/DTO/Requests/AuthenticateUserDTO.cs
+++ Src/FoodieAPI.Domain/DTO/Requests/AuthenticateUserDTO.cs
@@ -1,11 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace FoodieAPI.Domain.DTO.Requests;
-
-public class AuthenticateUserDTO
+namespace FoodieAPI.Domain.DTO.Requests
 {
-  [Required]
-  [EmailAddress]
-  public string userEmail;
-}
+  public class AuthenticateUserDTO(string email)
+  {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = email;
+  }
+};
+
